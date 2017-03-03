953699925269e855e1f5d0babfd970175f6d64b1
Marc Goujon <markitoxs@gmail.com>
2017-03-02 22:03:17 -0800

    Finaly first version working with encryption inside lvm

--- scripts/chroot.sh
+++ scripts/chroot.sh
@@ -3,45 +3,70 @@ HOST=jorgito
 USERNAME=markitoxs
 HOME_DIR="/home/${USERNAME}"
 SWAP_SIZE=4G
-echo DISK="$1", HOST="$HOST", USERNAME="$USERNAME", HOME_DIR="$HOME_DIR"
+
+# mount run to bypass lvmetadata errors
+mkdir /run/lvm
+mount --bind /hostrun/lvm /run/lvm
+
+# Add 'ext4' to MODULES
+# Add 'encrypt' and 'lvm2' to HOOKS before filesystems
+sudo sed -i 's/MODULES=""/MODULES="ext4"/g' /etc/mkinitcpio.conf
+sudo sed -i 's/filesystems keyboard fsck/encrypt lvm2 filesystems keyboard fsck/g' /etc/mkinitcpio.conf
+
+# regenerate 
+mkinitcpio -p linux
+
 # grub as a bootloader
-grub-install --target=i386-pc --recheck "$1"
-# This makes the grub timeout 0, it's faster than 5 :)
-sudo sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/g' /etc/default/grub
+grub-install --target=i386-pc --recheck /dev/sda
+
+# Customize grub config
+sudo sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=1/g' /etc/default/grub
+sudo sed -i 's@GRUB_CMDLINE_LINUX=""@GRUB_CMDLINE_LINUX="cryptdevice=/dev/sda3:luks:allow-discards"@g' /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
+
 # run these following essential service by default
 systemctl enable sshd.service
 systemctl enable dhcpcd.service
 systemctl enable ntpd.service
+
 echo "$HOST" > /etc/hostname
-# inject vimrc config to default user dir if you like vim
+
+## inject vimrc config to default user dir if you like vim
 echo -e 'runtime! archlinux.vim\nsyntax on' > /etc/skel/.vimrc
-# adding your normal user with additional wheel group so can sudo
+
+## adding your normal user with additional wheel group so can sudo
 useradd -m -G wheel -s /bin/bash "$USERNAME"
-# adding public key both to root and user for ssh key access
+
+## adding public key both to root and user for ssh key access
 mkdir -m 700 "$HOME_DIR/.ssh"
 mkdir -m 700 /root/.ssh
 cp /authorized_keys "/$HOME_DIR/.ssh"
 cp /authorized_keys /root/.ssh
 chown -R "$USERNAME:$USERNAME" "$HOME_DIR/.ssh"
-# adjust your timezone here
+
+## adjust your timezone here
 ln -f -s /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
 hwclock --systohc
+
 # adjust your name servers here if you don't want to use google
 echo 'name_servers="8.8.8.8 8.8.4.4"' >> /etc/resolvconf.conf
 echo en_US.UTF-8 UTF-8 > /etc/locale.gen
 echo LANG=en_US.UTF-8 > /etc/locale.conf
 locale-gen
+
 # because we are using ssh keys, make sudo not ask for passwords
 echo 'root ALL=(ALL) ALL' > /etc/sudoers
 echo '%wheel ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
+
 # I like to use vim :)
 echo -e 'EDITOR=vim' > /etc/environment
+
 # creating the swap file, if you have enough RAM, you can skip this step
 fallocate -l "$SWAP_SIZE" /swapfile
 chmod 600 /swapfile
 mkswap /swapfile
 echo /swapfile none swap defaults 0 0 >> /etc/fstab
+
 # auto-complete these essential commands
 echo complete -cf sudo >> /etc/bash.bashrc
 echo complete -cf man >> /etc/bash.bashrc
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,21 +1,87 @@
 #!/bin/bash
 DISK="/dev/$1"
-PARTITION="${DISK}1"
-echo DISK="$DISK", PARTITION="$PARTITION"
-parted -s "$DISK" mklabel msdos
-parted -s -a optimal "$DISK" mkpart primary ext4 0% 100%
-parted -s "$DISK" set 1 boot on
-mkfs.ext4 -F "$PARTITION"
+BOOT="${DISK}2"
+ROOT="${DISK}3"
+
+# Create a DOS partition table
+parted -s "$DISK" mklabel gpt
+
+# Create some partitions
+#+-----------+------+-------+-----------+
+#| Partition | Name | Size  | Flags     |
+#+-----------+------+-------+-----------+
+#| /dev/sda1 | grub | 2MB   | bios_grub |
+#| /dev/sda2 | boot | 200MB | boot      |
+#| /dev/sda3 | lvm  |       | lvm       |
+#+-----------+------+-------+-----------+
+# GPT
+parted $DISK --script -- mklabel gpt
+
+# GRUB
+parted $DISK --script -- mkpart primary 0MB 2MB
+parted $DISK --script -- name 1 bios_grub
+parted $DISK --script -- set 1 bios_grub
+
+# BOOT
+parted $DISK --script -- mkpart primary ext2 2MB 512MB
+parted $DISK --script -- name 2 boot
+parted $DISK --script -- set 2 boot on
+
+# Create ext2
+mkfs.ext2 /dev/sda2
+
+# LVM
+parted $DISK --script -- mkpart primary ext4 512MB 100%
+parted $DISK --script -- name 3 lvm
+parted $DISK --script -- set 3 lvm on
+
+echo 'Drive formatted as:'
+parted $DISK --script -- print
+
+
+# Encryption 
+cryptsetup --verbose --cipher aes-xts-plain64 --key-size 512 --hash sha512 --iter-time 5000 --use-random luksFormat "$ROOT"
+cryptsetup luksOpen "$ROOT" luks
+
+# Create encrypted partitions
+pvcreate /dev/mapper/luks
+vgcreate vg0 /dev/mapper/luks
+lvcreate --size 4G vg0 --name swap  #Change this depending on your ram
+lvcreate -l +100%FREE vg0 --name root
+
+# Create filesystems on encrypted partitions
+mkfs.ext4 /dev/mapper/vg0-root
+mkswap /dev/mapper/vg0-swap
+
+mount /dev/mapper/vg0-root /mnt # /mnt is the installed system
+swapon /dev/mapper/vg0-swap # Not needed but a good thing to test
+
+mkdir /mnt/boot
+mount "$BOOT" /mnt/boot
+
+# Prepare mirrors stuff
 # you can find your closest server from: https://www.archlinux.org/mirrorlist/all/
-echo 'Server = http://mirror.internode.on.net/pub/archlinux/$repo/os/$arch' > /etc/pacman.d/mirrorlist
-mount "$PARTITION" /mnt
+echo 'Server = http://mirrors.cat.pdx.edu/archlinux/$repo/os/$arch' > /etc/pacman.d/mirrorlist
 pacman -Syy
+
 # would recommend to use linux-lts kernel if you are running a server environment, otherwise just use "linux"
 pacstrap /mnt $(pacman -Sqg base) base-devel grub openssh sudo ntp wget vim
-genfstab -p /mnt >> /mnt/etc/fstab
+
+genfstab -pU /mnt >> /mnt/etc/fstab
+
+# Add tmp as a ramdisk
+echo "tmpfs	/tmp	tmpfs	defaults,noatime,mode=1777	0	0" >> /mnt/etc/fstab
+
+# Prepare to chroot
 cp ./chroot.sh /mnt
 cp ~/.ssh/authorized_keys /mnt
-arch-chroot /mnt ./chroot.sh "$DISK"
+echo "##################################################"
+echo "##################################################"
+echo "Entering the new system"
+# Enter the new system & Execute new stuff
+arch-chroot /mnt ./chroot.sh
+
+## Once done remove non necessary files and reboot
 rm /mnt/chroot.sh
 rm /mnt/authorized_keys
 umount -R /mnt
