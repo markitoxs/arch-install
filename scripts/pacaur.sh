fe9dcd75e72b79da704179f4dc6236ecbcadd4d6
Marc Goujon <markitoxs@gmail.com>
2017-03-10 21:46:42 -0800

    dotfiles in the right place now

--- scripts/pacaur.sh
+++ scripts/pacaur.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/env bash
 #
 echo '########### INSTALLING STUFF ############'
-pacaur -Sy --noedit --noconfirm i3-gaps rxvt-unicode stow
+cd
+pacaur -Sy --noedit --noconfirm i3-gaps i3status rxvt-unicode stow xterm
 pacaur -Sy --noedit --noconfirm virtualbox-guest-modules-arch virtualbox-guest-utils
 pacaur -Sy --noedit --noconfirm xorg-server xorg-xinit
+
+# Clone repo
+echo '############# Clone configs ##############'
+git clone https://github.com/markitoxs/.dotfiles.git
+cd .dotfiles
+stow zsh
+stow i3
+stow xresources
+stow vim
+
 echo '############# Create config files ##############'
 echo 'exec i3' >> /home/markitoxs/.xinitrc
-bash
 
-pacaur -Syu
+echo '############# Droping into a shell: ##############'
+
+bash
