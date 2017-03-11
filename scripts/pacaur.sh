cd15011b791c482877ba9050e5e31d168b006fe3
Marc Goujon <markitoxs@gmail.com>
2017-03-10 19:38:27 -0800

    added some dependencies

--- scripts/pacaur.sh
+++ scripts/pacaur.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
-pacaur -Syu
+#
+echo '########### INSTALLING STUFF ############'
 pacaur -Sy --noedit --noconfirm i3-gaps rxvt-unicode stow
+pacaur -Sy --noedit --noconfirm virtualbox-guest-modules-arch virtualbox-guest-utils
+pacaur -Sy --noedit --noconfirm xorg-server xorg-xinit
+echo '############# Create config files ##############'
+echo 'exec i3' >> /home/markitoxs/.xinitrc
 bash
+
 pacaur -Syu
