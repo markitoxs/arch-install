acdb8441fed1658abfab42cea0da7c1ef747bb50
Marc Goujon <markitoxs@gmail.com>
2018-05-03 17:13:19 +0000

    added debian file

---
+++ container/debian.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -e
+
+# Install VSCODE repo
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+sudo apt-get update
+sudo apt-get install -y code # or code-insiders
+
+
+# Install git 
+sudo apt-get install -y git
+
+# Install terraform
+curl -O terraform.zip https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip
+unzip terraform.zip
+mkdir -p ./local/bin
+mv terraform ./local/bin
+
+PATH="/home/markitoxs/.local/bin/:$PATH"
+export PATH
+
+# Install zsh
+sudo apt-get install -y zsh
+chsh $(whoami) -s $(which zsh)
+
+
+ca 
